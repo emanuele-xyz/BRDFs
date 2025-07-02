93509cd71c3bf05df921690a502b4671916994d0
emanuele franchi <me.emanuele.xyz@gmail.com>
2025-07-02 21:41:51 +0200

    implement basic custom assertions

--- Main.cpp
+++ Main.cpp
@@ -1,6 +1,34 @@
-#include <print>
+// ---------- Standard Library Includes ----------
 
-int main(void)
+#include <format>
+#include <iostream>
+#include <stacktrace>
+#include <stdexcept>
+
+// ---------- Assertions ----------
+
+#define Crash(msg) throw std::runtime_error{ std::format("[CRASH]: {}\n{}", msg, std::stacktrace::current()) };
+#define Check(p) do { if (!(p)) Crash("Assertion failed: " #p); } while (false)
+
+// ---------- Entry Point ----------
+
+static void Entry()
 {
-    std::print("Hello!");
+    Check(false);
+}
+
+// ---------- Main ----------
+
+int main()
+{
+    try
+    {
+        Entry();
+    }
+    catch (const std::runtime_error& e)
+    {
+        std::cout << e.what() << "\n";
+    }
+
+    return 0;
 }
